58b3238c84739f29b2f059acf83f098c0b615ad7
tanishk agrawal <tanishk7676@gmail.com>
2026-08-12 16:08:09 +0530

    number analyzer

---
+++ hello.cpp
@@ -0,0 +1,32 @@
+//Number Analyzer
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n ,a,b,c,d , sum =0;
+    cout << "Enter a three digit number: " ;
+    cin >> n ;
+    if ( n % 2 == 0) {
+        cout << n << " is an even number." << endl ;
+    } else {
+        cout << n << " is an odd number." << endl ;
+    }
+
+    if ( n > 0 ){
+        cout << n << " is a posetive integer" << endl ;
+    } else if (n < 0){
+        cout << n << " is a negetive integer" << endl ;
+    } else {
+        cout << n << " is zero" << endl ;
+    }
+
+    if(n>0){
+         a = n / 100;
+         b = n %100;
+         c = b %10;
+         d =b/10;
+        sum = a +  c + d ;
+        cout << "sum of all digits of " << n << " is " << sum << endl ;
+    }
+    return 0 ;
+}
