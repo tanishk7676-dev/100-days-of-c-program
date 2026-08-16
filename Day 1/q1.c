b0e095f07e56b49f332f32a60519ebc4f2c65e8f
tanishk7676-dev <tanishk7676@gmail.com>
2026-08-16 22:55:45 +0530

    Day 1

---
+++ Day 1/q1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int n1 , n2 ,sum ;
+    printf("enter any two no \n");
+    scanf(" %d %d",&n1 ,&n2);
+    sum = n1 + n2 ;
+    printf("the sum of two no. %d and %d is %d ", n1 , n2 , sum);
+    return 0;
+
+}
---
+++ Day 1/q2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    int n1 , n2 , sum , diff , m , div ;
+    printf(" enter any two mumbers ");
+    scanf("  %d %d",&n1 ,&n2 );
+    sum = n1 + n2 ;
+    diff = n1 - n2 ;
+    m = n1*n2 ;
+    if( n2 != 0){
+        div = n1/n2 ;
+    } else {
+        printf( " zero division is not defined");
+    }
+    printf(" sum = %d , diffrence = %d , multipley = %d , division = %d", sum , diff , m ,div );
+    return 0 ;
+    
+}
